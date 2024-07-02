fa60673851d307902c486ec016eac7db11c2846f
Vitali Khulup <56646052+limanor44@users.noreply.github.com>
2024-07-02 10:53:25 +0300

    Initial

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -2,7 +2,5 @@ package main.java.ru.clevertec.check;
 
 public class CheckRunner {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-        }
     }
 }
