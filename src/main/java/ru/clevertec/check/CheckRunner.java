105900f99e9af7376daccae0bde034ba564e3a60
Vitali Khulup <56646052+limanor44@users.noreply.github.com>
2024-07-02 10:46:10 +0300

    Initial commit

---
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -0,0 +1,8 @@
+package main.java.ru.clevertec.check;
+
+public class CheckRunner {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+        }
+    }
+}
